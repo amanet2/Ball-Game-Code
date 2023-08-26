929ef7635d0c388dc51270992672bde2cbfa3fc9
amanet2 <tonyjmstallion@gmail.com>
2023-08-26 12:51:21 -0500

    some vars

--- src/xMain.java
+++ src/xMain.java
@@ -2,7 +2,11 @@ import things.item;
 import items.foobar;
 
 public class xMain {
+    static int SERVERVAR_COUNT = 128;
+    static int PORT_LISTEN = 0;
+    static String[] serverVars = new String[MAX_STARTUPVAR_COUNT];
     public static void main(String[] args) {
+        serverVars[PORT_LISTEN] = "5555";
         item test = new item();
         item test2 = new foobar();
         item[] items = {test, test2};
