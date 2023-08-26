4adb9583b4c2098258259b06ad62d2518ff155fd
amanet2 <tonyjmstallion@gmail.com>
2023-08-26 13:11:26 -0500

    move some code and demonstrate pattern

--- src/xMain.java
+++ src/xMain.java
@@ -2,11 +2,10 @@ import things.item;
 import items.foobar;
 
 public class xMain {
-    static int SERVERVAR_COUNT = 128;
-    static int PORT_LISTEN = 0;
-    static String[] serverVars = new String[MAX_STARTUPVAR_COUNT];
     public static void main(String[] args) {
-        serverVars[PORT_LISTEN] = "5555";
+        server.bindings[server.PORT_LISTEN] = "sv_port_listen";
+        server.vars[server.PORT_LISTEN] = "5555";
+
         item test = new item();
         item test2 = new foobar();
         item[] items = {test, test2};
