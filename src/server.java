dfa66daa2df17ab75fa2fc2ed48285b924c583ba
amanet2 <tonyjmstallion@gmail.com>
2023-08-26 13:20:28 -0500

    reset

--- src/server.java
+++
@@ -1,8 +0,0 @@
-public class server {
-    static int MAX_VARS = 128;
-    static int PORT_LISTEN = 0;
-
-    static String[] vars = new String[MAX_VARS];
-    static String[] bindings = new String[MAX_VARS];
-
-}
--- src/xMain.java
+++ src/xMain.java
@@ -3,9 +3,6 @@ import items.foobar;
 
 public class xMain {
     public static void main(String[] args) {
-        server.bindings[server.PORT_LISTEN] = "sv_port_listen";
-        server.vars[server.PORT_LISTEN] = "5555";
-
         item test = new item();
         item test2 = new foobar();
         item[] items = {test, test2};
